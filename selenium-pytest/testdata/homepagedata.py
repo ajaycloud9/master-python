fdbf2abf9b9cbf24b233f24f94362922c9aaa628
Ajay Singh <singh.ajay@husky.neu.edu>
2024-07-16 21:11:52 -0700

    Adding the real time selenium with pytest project

---
+++ selenium-pytest/testdata/homepagedata.py
@@ -0,0 +1,11 @@
+
+from testdata.exceldata import ExcelData
+
+class HomePageData(ExcelData):
+    
+    #Static data
+    home_data =[{'firstname':'Ajay','lastname': "singh"}, {'firstname':'vijay','lastname': "test"}]
+
+    # Also inherting the Excel data to get the data from there based on excel
+
+
